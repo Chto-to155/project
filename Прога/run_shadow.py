d9d341d1aabae5b0d2f5b5e8493117209f750c27
Chto-to155 <griv0chka@mail.ru>
2025-06-23 11:41:32 +0300

    Update run_shadow.py

--- Прога/run_shadow.py
+++ Прога/run_shadow.py
@@ -11,9 +11,11 @@ try:
         print("=============================================================")
         print(f"Начало работы с полиэдром '{name}'")
         start_time = time()
-        Polyedr(f"data/{name}.geom").draw(tk)
+        figure = Polyedr(f"data/{name}.geom")
+        figure.draw(tk)
         delta_time = time() - start_time
         print(f"Изображение полиэдра '{name}' заняло {delta_time} сек.")
+        print(f"Общая длина полностью видимых рёбер с нужным параметром: '{figure.full_len()}'")
         input("Hit 'Return' to continue -> ")
 except (EOFError, KeyboardInterrupt):
     print("\nStop")
